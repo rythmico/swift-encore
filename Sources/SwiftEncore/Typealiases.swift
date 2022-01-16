77b4c6d02f77a729daa81f9924d75aa40520eff3
davdroman <d@vidroman.dev>
2022-01-16 02:55:52 +0000

    Add TODO

--- Sources/SwiftEncore/Typealiases.swift
+++ Sources/SwiftEncore/Typealiases.swift
@@ -1,3 +1,3 @@
 public typealias Action = () -> Void
-public typealias Handler<T> = (T) -> Void
+public typealias Handler<T> = (T) -> Void // TODO: use variadic generics when available
 public typealias ResultHandler<Success, Failure: Error> = Handler<Result<Success, Failure>>
